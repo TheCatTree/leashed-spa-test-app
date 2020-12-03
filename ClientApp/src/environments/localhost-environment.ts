7a01a6fd4f2c3faec000ad23565c3d4e1b7ad540
Steven Hartley <steven.c.hartley@gmail.com>
2020-12-03 20:11:44 +1300

    removing local host from git

--- ClientApp/src/environments/localhost-environment.ts
+++
@@ -1,5 +0,0 @@
-export const localhostEnvironment: {[key: string]: string} = {
-
-  MYAPP_DEFAULT_SETTING: 'this is a default that might be overwritten by an environment var of same name',
-  MYAPP_GOOGLE_MAP_API_KEY: 'AIzaSyDx9FPEPKrzWPUN12UkmDy9cGvj9Uw6LAU'
-};
