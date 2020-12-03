b70d9ba3611f07f4990ef009a6aa68dedbc0335d
Steven Hartley <steven.c.hartley@gmail.com>
2020-12-03 20:56:09 +1300

    Fixing api Key

--- ClientApp/src/environments/environment-variables.ts
+++ ClientApp/src/environments/environment-variables.ts
@@ -3,5 +3,4 @@ import { localhostEnvironment } from './localhost-environment';
 
 declare let ENV_VARS: {[key: string] : string};
 
-export const Environment = Object.assign(localhostEnvironment,
-  ENV_VARS);
+export const Environment = Object.assign(localhostEnvironment,ENV_VARS);
