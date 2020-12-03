69d4cd6927be8f7b613a8d58b98c1e41d55e3a7b
Steven Hartley <steven.c.hartley@gmail.com>
2020-12-03 20:12:19 +1300

    removing localhost-env from repo

---
+++ ClientApp/src/environments/localhost-environment.ts
@@ -0,0 +1,5 @@
+export const localhostEnvironment: {[key: string]: string} = {
+
+  MYAPP_DEFAULT_SETTING: 'this is a default that might be overwritten by an environment var of same name',
+  MYAPP_GOOGLE_MAP_API_KEY: 'AIzaSyDx9FPEPKrzWPUN12UkmDy9cGvj9Uw6LAU'
+};
